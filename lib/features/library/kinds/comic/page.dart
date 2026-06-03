236e2137830c898704530b185fd692c1d2643239
SaiTatter <andrei.voicu133@gmail.com>
2026-06-03 03:36:35 +0300

    Localize workspace normalization and kind pages

--- lib/features/library/kinds/comic/page.dart
+++ lib/features/library/kinds/comic/page.dart
@@ -1,28 +1,12 @@
-import 'package:collectarr_app/features/library/config/library_type_config.dart';
 import 'package:collectarr_app/features/library/generic/page.dart';
-import 'package:flutter/material.dart';
 
-class ComicLibraryPage extends StatelessWidget {
+class ComicLibraryPage extends LibraryPage {
   const ComicLibraryPage({
     super.key,
-    required this.type,
-    required this.topBar,
-    required this.accent,
-    required this.routeUri,
-  });
-
-  final LibraryTypeConfig type;
-  final Widget topBar;
-  final Color accent;
-  final Uri routeUri;
-
-  @override
-  Widget build(BuildContext context) {
-    return LibraryPage(
-      type: type,
-      topBar: topBar,
-      accent: accent,
-      routeUri: routeUri,
-    );
-  }
+    required super.type,
+    required super.topBar,
+    required super.accent,
+    required super.routeUri,
+  }) : super(
+        );
 }
