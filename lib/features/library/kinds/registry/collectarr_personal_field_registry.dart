b9182bc30ada7e2cd88b3697dedbecb4b361ebd5
Voicu Andrei-Daniel <andrei.voicu133@gmail.com>
2026-09-22 04:51:41 +0300

    fix(registry): wire all personal field contributors

--- lib/features/library/kinds/registry/collectarr_personal_field_registry.dart
+++ lib/features/library/kinds/registry/collectarr_personal_field_registry.dart
@@ -1,6 +1,12 @@
 import 'package:collectarr_app/core/models/catalog_media_kind.dart';
+import 'package:collectarr_app/features/library/kinds/anime/anime_module.dart';
+import 'package:collectarr_app/features/library/kinds/book/book_module.dart';
 import 'package:collectarr_app/features/library/kinds/comic/comic_module.dart';
 import 'package:collectarr_app/features/library/kinds/game/game_module.dart';
+import 'package:collectarr_app/features/library/kinds/manga/manga_module.dart';
+import 'package:collectarr_app/features/library/kinds/movie/movie_module.dart';
+import 'package:collectarr_app/features/library/kinds/music/music_module.dart';
+import 'package:collectarr_app/features/library/kinds/tv/tv_module.dart';
 import 'package:collectarr_app/features/library/metadata/library_field_ownership.dart';
 import 'package:collectarr_app/features/library/metadata/library_personal_field_contributor.dart';
 
