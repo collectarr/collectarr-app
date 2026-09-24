d3fc1893c2a15ee36b53bcc8928981a4b78f8cc3
Voicu Andrei-Daniel <andrei.voicu133@gmail.com>
2026-09-24 16:49:52 +0300

    test(app): update shell import path

--- integration_test/app_smoke_test.dart
+++ integration_test/app_smoke_test.dart
@@ -16,7 +16,7 @@ import 'package:collectarr_app/features/library/providers/media_catalog_provider
 import 'package:collectarr_app/state/auth_provider.dart';
 import 'package:collectarr_app/features/sync/state/sync_controller.dart';
 import 'package:collectarr_app/features/sync/state/sync_state.dart';
-import 'package:collectarr_app/ui/app_shell.dart';
+import 'package:collectarr_app/app/app_shell.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_riverpod/misc.dart' show Override;
